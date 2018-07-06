c6cf390db58785b6803f68882e5bb453847e9631
anilad <aniladdao@gmail.com>
2018-07-06 16:43:14 -0700

    Updates to admin forms and lists

--- client/src/components/AdminComponents/CreateProduct/CreateProduct.js
+++ client/src/components/AdminComponents/CreateProduct/CreateProduct.js
@@ -1,22 +1,31 @@
 import React from 'react';
 import { Container, Row, Col, Button, Input } from 'mdbreact';
 
-// considering moving from using mdbreact for forms to using basic bootstrap to style our own way
+// Edit code to prepopulate fields with values if updating
 
 const createProduct = () => {
     return (
         <Container>
+            <h1>Create Product</h1>
             <Row>
                 <Col md="1" />
-                <Col md="10">
-                    <form>
-                        <p className="h5 text-center mb-4">Create Product</p>
-                        <div className="grey-text">
-                            <Input label="Product Name" id="productName " type="text" validate error="wrong" success="right" />
-                            <Input type="textarea" label="Product Description" id="productDescription" validate error="wrong" success="right" />
-                            <Input type="text" label="Price" id="productPrice" validate error="wrong" success="right" />
-                            <label htmlFor="productSize">Product Size</label>
-                            <select id="productSize" className="ml-2">
+                <Col md="10 text-left">
+                    <form className="grey-text">
+                        <div class="form-group">
+                            <label htmlFor="productName">Product Name</label>
+                            <input type="text" class="form-control" id="productName" placeholder="Enter Product Name" />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="productDescription">Product Description</label>
+                            <textarea className="form-control" id="productDescription" rows="3" placeholder="Enter Product Description"></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label htmlFor="productPrice">Product Price</label>
+                            <input type="text" class="form-control" id="productPrice" placeholder="Enter Product Price" />
+                        </div>
+                        <div class="form-group">
+                            <label for="productSize">Product Size</label>
+                            <select class="form-control" id="productSize">
                                 <option value="one-size">One Size</option>
                                 <option value="x-small">X-Small</option>
                                 <option value="small">Small</option>
@@ -25,11 +34,10 @@ const createProduct = () => {
                                 <option value="x-large">X-Large</option>
                                 <option value="2x-large">2X-Large</option>
                             </select>
-                            <br/>
-                            <label htmlFor="productImage">Product Image</label>
-                            <input className="ml-2" type="file"
-                                id="productImage"
-                                accept="image/png, image/jpeg" />
+                        </div>
+                        <div className="custom-file mb-3">
+                            <label class="custom-file-label" htmlFor="productImage">Upload Product Image</label>
+                            <input type="file" className="custom-file-input" id="productImage" accept="image/png, image/jpeg"/>
                         </div>
                         <div className="text-center mt-2">
                             <Button className="btn-block">Create Product</Button>
