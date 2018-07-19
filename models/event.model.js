4b0e581d7ba97371cf4c86ddf4f3a8ad5748e633
Keith Nguyen <khoacn@gmail.com>
2018-07-19 15:31:34 -0700

    added backend validations

--- models/event.model.js
+++ models/event.model.js
@@ -3,21 +3,38 @@ module.exports = (connection, Sequelize) =>{
         title: {
             type: Sequelize.STRING,
             allowNull: false,
+            unique: true,
+            validate:{
+                len:[6]
+            }
         },
         date:{
-            type: Sequelize.DATE
+            type: Sequelize.DATE,
+            validate:{
+                isAfter: Date.now().toString()
+            }
         },
         time:{
             type: Sequelize.TIME
         },
+        // end time
         location:{
             type: Sequelize.STRING,
+            validate:{
+                len:[15]
+            }
         },
         description:{
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            validate:{
+                len:[50,1000]
+            }
         },
         event_photo:{
             type: Sequelize.BLOB,
+            validate:{
+                //find type of file to validate. Nothing but jpeg
+            }
         },
         map_photo:{
             type: Sequelize.BLOB
--- models/user.model.js
+++ models/user.model.js
@@ -2,20 +2,30 @@
 module.exports = (connection, Sequelize) =>{
     const User = connection.define('user', {
         first_name: {
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            validate:{
+                len:[2]
+            }
         },
         last_name:{
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            validate:{
+                len:[2]
+            }
         },
         email:{
             type: Sequelize.STRING,
             unique: true,
         },
         phone_number:{
-            type: Sequelize.INTEGER
+            type: Sequelize.INTEGER,
+            validate:{
+                len:[10]
+            }
         },
         user_name:{
-            type: Sequelize.STRING
+            type: Sequelize.STRING,
+            unique:true
         },
         password:{
             type: Sequelize.STRING
